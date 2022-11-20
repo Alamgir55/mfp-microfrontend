d12a1e6b013b2b4d7bbfb707bc3cdf64099e9fbd
Alamgir55 <alamgir555hossain@gmail.com>
2022-11-20 07:09:10 -0500

    auth-container

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -19,7 +19,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   return {
     onParentNaviage({ pathname: nextPathname }) {
       const { pathname } = history.location;
-
+      console.log(nextPathname);
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
@@ -28,7 +28,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 }
 
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
